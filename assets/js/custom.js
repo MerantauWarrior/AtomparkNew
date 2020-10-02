3c1617204da21238974fea48b496f718e25b3833
Boris <boris1488ss@gmail.com>
2020-10-02 10:23:16 +0300

    commit buy

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,6 +1,105 @@
-$(document).ready(function () {
-  var language = $('html').attr('lang');
+//Select
+var x, i, j, selElmnt, a, b, c, img;
+x = document.getElementsByClassName("select");
+for (i = 0; i < x.length; i++) {
+  selElmnt = x[i].getElementsByTagName("select")[0];
+  a = document.createElement("DIV");
+  a.setAttribute("class", "select-selected");
+  if (selElmnt.options[selElmnt.selectedIndex].getAttribute('data-icon') !== null) {
+    a.innerHTML = '<img src="' + selElmnt.options[selElmnt.selectedIndex].getAttribute('data-icon') + '" alt=""><span>' + selElmnt.options[selElmnt.selectedIndex].innerHTML + '</span>';
+  } else {
+    a.innerHTML = selElmnt.options[selElmnt.selectedIndex].innerHTML;
+  }
+  if (selElmnt.disabled) {
+    x[i].classList.add('select--disabled');
+  }
+  x[i].appendChild(a);
+  b = document.createElement("DIV");
+  b.setAttribute("class", "select-items select-hide");
+  for (j = 1; j < selElmnt.length; j++) {
+    c = document.createElement("DIV");
+    if (selElmnt.options[j].getAttribute('data-icon') !== null) {
+      c.innerHTML = '<img src="' + selElmnt.options[j].getAttribute('data-icon') + '" alt=""><span>' + selElmnt.options[j].innerHTML + '</span>';
+      c.setAttribute('data-val',selElmnt.options[j].getAttribute('value'));
+    } else {
+      c.innerHTML = selElmnt.options[j].innerHTML;
+      c.setAttribute('data-val',selElmnt.options[j].getAttribute('value'));
+    }
+    c.addEventListener("click", function (e) {
+      var y, i, k, s, h;
+      s = this.parentNode.parentNode.getElementsByTagName("select")[0];
+      h = this.parentNode.previousSibling;
+      for (i = 0; i < s.length; i++) {
+        if (s.options[i].innerHTML == this.textContent) {
+          s.selectedIndex = i;
+          h.innerHTML = this.innerHTML;
+          h.setAttribute('data-val',s.options[i].getAttribute('value'));
+          y = this.parentNode.getElementsByClassName("same-as-selected");
+          for (k = 0; k < y.length; k++) {
+            y[k].removeAttribute("class");
+          }
+          this.setAttribute("class", "same-as-selected");
+          break;
+        }
+      }
+      h.click();
+    });
+    b.appendChild(c);
+  }
+  x[i].appendChild(b);
+  if (selElmnt.disabled) {
+    console.log('disabled');
+  } else {
+    a.addEventListener("click", function (e) {
+      e.stopPropagation();
+      closeAllSelect(this);
+      this.nextSibling.classList.toggle("select-hide");
+      this.classList.toggle("select-arrow-active");
+    });
+  }
+}
+function closeAllSelect(elmnt) {
+  var x, y, i, arrNo = [];
+  x = document.getElementsByClassName("select-items");
+  y = document.getElementsByClassName("select-selected");
+  for (i = 0; i < y.length; i++) {
+    if (elmnt == y[i]) {
+      arrNo.push(i)
+    } else {
+      y[i].classList.remove("select-arrow-active");
+    }
+  }
+  for (i = 0; i < x.length; i++) {
+    if (arrNo.indexOf(i)) {
+      x[i].classList.add("select-hide");
+    }
+  }
+}
+document.addEventListener("click", closeAllSelect);
 
+
+(function($) {
+// Only nambers
+  $.fn.inputFilter = function(inputFilter) {
+    return this.on("input keydown keyup mousedown mouseup select contextmenu drop", function() {
+      if (inputFilter(this.value)) {
+        this.oldValue = this.value;
+        this.oldSelectionStart = this.selectionStart;
+        this.oldSelectionEnd = this.selectionEnd;
+      } else if (this.hasOwnProperty("oldValue")) {
+        this.value = this.oldValue;
+        this.setSelectionRange(this.oldSelectionStart, this.oldSelectionEnd);
+      } else {
+        this.value = "";
+      }
+    });
+  };
+
+  var language = $('html').attr('lang');
+// Only numbers
+  $(".js-number").inputFilter(function(value) {
+    return /^\d*$/.test(value);
+  });
 // Timer
   if($('.js-timer').length>0){
     var countDownDate = new Date('October 7, 2020 03:24:00').getTime();
@@ -27,4 +126,4 @@ $(document).ready(function () {
     setInterval(timer, 1000);
   }
 
-});
+}(jQuery));
